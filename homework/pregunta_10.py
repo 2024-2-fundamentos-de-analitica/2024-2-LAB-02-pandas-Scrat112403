dbf632683b2b5ec3618dca13849f0490086a1c78
Scrat112403 <jacarmonaa@unal.edu.co>
2025-01-22 20:45:31 -0800

    Done

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_10():
     """
@@ -20,3 +21,12 @@ def pregunta_10():
     D                   1:2:3:5:5:7
     E   1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    group = df.groupby("c1")["c2"].apply(lambda x: ":".join(map(str, sorted(x))))
+
+    result = group.reset_index(name="c2").set_index("c1")
+
+    return result
+
+
+print(pregunta_10())
